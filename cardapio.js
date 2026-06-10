3ba83bdd7c4a66dd2318981f4d434a458abe34f7
Kadu2026 <rodriguesdasilvacarlos444@gmail.com>
2026-06-09 21:40:57 -0300

    Alguns js no cardápio

--- cardapio.js
+++ cardapio.js
@@ -9,3 +9,23 @@ function adicionarItem(botao) {
         botao.style.backgroundColor = "green";
     }
 }
+
+function mostrarDetalhes(imagem) {
+
+    const box = imagem.parentElement;
+
+    const detalhes = box.querySelector(".detalhes");
+    const maisVendido = box.querySelector(".mais-vendido");
+
+    if (detalhes.style.display === "block") {
+
+        detalhes.style.display = "none";
+        maisVendido.style.display = "block";
+
+    } else {
+
+        detalhes.style.display = "block";
+        maisVendido.style.display = "none";
+
+    }
+}
