cf59460266cb86711667af378815b98bbf8b5b24
Marcos Ray <marcosray17@gmail.com>
2026-06-14 15:07:50 -0300

    finalizado

--- cardapio.js
+++ cardapio.js
@@ -9,14 +9,14 @@ function adicionarItem(botao) {
         botao.classList.remove("adicionado");
         botao.textContent = "Adicionar";
 
-        mensagem.textContent = "❌ Removido do carrinho";
+        mensagem.textContent = " Removido do carrinho";
 
     } else {
 
         botao.classList.add("adicionado");
         botao.textContent = "✓ Adicionado";
 
-        mensagem.textContent = "✅ Adicionado ao carrinho";
+        mensagem.textContent = " Adicionado ao carrinho";
 
     }
 
@@ -26,3 +26,19 @@ function adicionarItem(botao) {
         mensagem.classList.remove("show");
     }, 2000);
 }
+
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+},{
+    threshold: 0.15
+});
+
+reveals.forEach(reveal => {
+    observer.observe(reveal);
+});
